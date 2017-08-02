e4c3cb2d06dd3ae9de287f88071987e9c11fa0bf
zeroluck <deathbyninja@gmail.com>
2017-08-02 15:24:44 -0500

    Spelling error in command

--- Ch03/Ch03/SQL-ConfigureClusterNetworkAdapter.ps1
+++ Ch03/Ch03/SQL-ConfigureClusterNetworkAdapter.ps1
@@ -1,5 +1,5 @@
 Get-ClusterNetwork | fl * 
 $clusadapt = Get-ClusterNetwork -Cluster LSSPSQLClus -Name "Cluster Network 1" 
-$clusadap.Name = "Intranet" 
+$clusadapt.Name = "Intranet" 
 $clusadapt = Get-ClusterNetwork -Cluster LSSPSQLClus -Name "Cluster Network 2" 
-$clusadap.Name = "Heartbeat" 
+$clusadapt.Name = "Heartbeat" 
